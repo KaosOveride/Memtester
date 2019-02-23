42af59dbf887ab21eb921ec5edb64f7c3da3c177
KaosOveride <kaosoveride@gmail.com>
2019-02-22 17:42:17 -0800

    Added code comments

--- Testing Game/TestGame.cpp
+++ Testing Game/TestGame.cpp
@@ -15,18 +15,18 @@ int main() {
 	int*** ptr2ptr2(&ptr2ptr);
 
 	do {
-		cout << "Process ID: " << dec << GetCurrentProcessId() << endl;
+		cout << "Process ID: " << dec << GetCurrentProcessId() << endl;//using Windows.h to get current process id
 		cout << endl;
-		cout << "varInt       (0x" << hex << uppercase << &varInt << ") = " << dec << varInt << endl;
-		cout << "varString    (0x" << hex << uppercase << &varString << ") = " << varString << endl;
-		cout << "arrChar[" << dec << CHAR_ARRAY_SIZE << "] (0x" << hex << uppercase << &arrChar << ") = " << arrChar << endl;
+		cout << "varInt       (0x" << hex << uppercase << &varInt << ") = " << dec << varInt << endl;//getting/displaying the address of varInt
+		cout << "varString    (0x" << hex << uppercase << &varString << ") = " << varString << endl; //getting/displaying address of varString
+		cout << "arrChar[" << dec << CHAR_ARRAY_SIZE << "] (0x" << hex << uppercase << &arrChar << ") = " << arrChar << endl;//getting/displaying address of arrChar
 		cout << endl;
-		cout << "ptr2int      (0x" << hex << uppercase << &ptr2int << ") = 0x" << ptr2int << endl;
-		cout << "ptr2ptr      (0x" << hex << uppercase << &ptr2ptr << ") = 0x" << ptr2ptr << endl;
-		cout << "ptr2ptr2     (0x" << hex << uppercase << &ptr2ptr2 << ") = 0x" << ptr2ptr2 << endl;
+		cout << "ptr2int      (0x" << hex << uppercase << &ptr2int << ") = 0x" << ptr2int << endl;//getting/displaying address of pointer 1
+		cout << "ptr2ptr      (0x" << hex << uppercase << &ptr2ptr << ") = 0x" << ptr2ptr << endl;//getting/displaying of pointer 2
+		cout << "ptr2ptr2     (0x" << hex << uppercase << &ptr2ptr2 << ") = 0x" << ptr2ptr2 << endl;//getting/displaying of pointer 3
 		cout << endl;
 		cout << "Press ENTER to print again." << endl;
-		getchar();
+		getchar();//waiting for enter key
 		cout << endl << "---------------------------------------------------" << endl << endl;
 	} while (true);
 
